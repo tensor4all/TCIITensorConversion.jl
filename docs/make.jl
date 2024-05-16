6039ea0810817f1c808f8a3ef734401ee9f4153b
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2024-05-16 18:52:46 +0200

    Update docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,22 @@
-using TCIITensorConversion
+using QuanticsGrids
 using Documenter
 
-DocMeta.setdocmeta!(TCIITensorConversion, :DocTestSetup, :(using TCIITensorConversion); recursive=true)
+DocMeta.setdocmeta!(QuanticsGrids, :DocTestSetup, :(using QuanticsGrids); recursive=true)
 
 makedocs(;
-    modules=[TCIITensorConversion],
+    modules=[QuanticsGrids],
     authors="Ritter.Marc <Ritter.Marc@physik.uni-muenchen.de> and contributors",
-    repo="https://gitlab.com/Ritter.Marc/TCIITensorConversion.jl/blob/{commit}{path}#{line}",
-    sitename="TCIITensorConversion.jl",
+    sitename="QuanticsGrids.jl",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://Ritter.Marc.gitlab.io/TCIITensorConversion.jl",
+        canonical="https://github.com/tensor4all/TCIITensorConversion.jl",
         edit_link="main",
-        assets=String[],
-    ),
+        assets=String[]),
     pages=[
         "Home" => "index.md",
-    ],
+        "API Reference" => "apireference.md",
+    ])
+
+deploydocs(;
+    repo="github.com/tensor4all/TCIITensorConversion.jl.git",
+    devbranch="main",
 )
