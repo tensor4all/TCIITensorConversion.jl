af554329a6a617ca5fc708643eea40a35ec5c9f0
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2024-11-16 10:08:12 +0900

    Use ITensorMPS package

--- src/TCIITensorConversion.jl
+++ src/TCIITensorConversion.jl
@@ -3,9 +3,9 @@ module TCIITensorConversion
 import TensorCrossInterpolation as TCI
 using ITensors
 using Reexport: @reexport
+import ITensorMPS
 @reexport using ITensorMPS: MPS
 
-export MPS
 export evaluate_mps
 
 include("ttmpsconversion.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
 using ITensors
+using ITensors.SiteTypes: siteinds
+using ITensorMPS
 import TensorCrossInterpolation as TCI
 using TCIITensorConversion
 using Test
